8364b936edb90c5606c18e66473397875e6db4cb
Rosalia Hernandez <rosalia254@gmail.com>
2018-11-13 18:11:25 -0700

    Update restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -89,8 +89,6 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.srcset = DBHelper.imageSrcsetForRestaurant(restaurant);
-  image.sizes = DBHelper.imageSizesForRestaurant(restaurant); 
   image.alt = `Picture of ${restaurant.name}`;
 
 
